67ec10577d6e44bae568f141d77960c30f3d997a
Brandon Skari <brandon@skari.org>
2014-05-20 19:00:58 -0600

    Add start/stop buttons, and send messages over UDP

--- android/src/com/example/sparkfun_avc/MainActivity.java
+++ android/src/com/example/sparkfun_avc/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.sparkfun_avc;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
@@ -72,13 +76,17 @@ public class MainActivity extends ActionBarActivity {
         }
     }
     
-    public void sendSensorData(View view) {
+    private InetAddress getAddress() throws UnknownHostException {
     	EditText editText = (EditText) findViewById(R.id.edit_server_ip);
     	final String server = editText.getText().toString();
+    	return InetAddress.getByName(server);
+    }
+    
+    public void sendSensorData(View view) {
     	if (sensorDumpThread == null || !sensorDumpThread.isAlive()) {
     		InetAddress address;
     		try {
-    			address = InetAddress.getByName(server);
+    			address = getAddress();
     		} catch (UnknownHostException uhe) {
     			AlertDialog.Builder builder = new AlertDialog.Builder(this);
     			builder.setMessage(R.string.server_not_found);
@@ -87,7 +95,7 @@ public class MainActivity extends ActionBarActivity {
     			return;
     		}
     		
-    		byte[] buffer = "\"{request_response\": true}".getBytes();
+    		byte[] buffer = "{\"requestResponse\": true}".getBytes();
     		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 8384);
     		DatagramSocket socket;
 			try {
@@ -103,14 +111,18 @@ public class MainActivity extends ActionBarActivity {
 			try {
 				socket.send(packet);
 			} catch (IOException ioe) {
-    			socket.close();
     			AlertDialog.Builder builder = new AlertDialog.Builder(this);
     			builder.setMessage(R.string.socket_error);
     			AlertDialog dialog = builder.create();
     			dialog.show();
     			return;
+			} finally {
+				socket.close();
 			}
-    		sensorDumpThread = new SensorDumpThread(socket);
+			
+			// TODO: Wait for a response before making this thread
+			
+    		//sensorDumpThread = new SensorDumpThread(socket);
     	}
     }
     
@@ -118,5 +130,65 @@ public class MainActivity extends ActionBarActivity {
     	if (sensorDumpThread != null && sensorDumpThread.isAlive()) {
     		sensorDumpThread.stopDumping();
     	}
+    }
+    
+    private void runOrStop(final boolean run) {
+    	if (run && (sensorDumpThread == null || !sensorDumpThread.isAlive())) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(R.string.not_running);
+			AlertDialog dialog = builder.create();
+			dialog.show();
+			return;
+    	}
+    	
+		InetAddress address;
+		try {
+			address = getAddress();
+		} catch (UnknownHostException uhe) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(R.string.server_not_found);
+			AlertDialog dialog = builder.create();
+			dialog.show();
+			return;
+		}
+		
+		byte[] buffer = String.format(
+			"{\"requestResponse\": true," +
+					" \"type\": \"command\"," + 
+					" \"command\": \"%s\"}",
+			run ? "run" : "stop"
+		).getBytes();
+		
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 8384);
+		DatagramSocket socket;
+		try {
+			socket = new DatagramSocket();
+		} catch (SocketException se) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(R.string.create_socket_error);
+			AlertDialog dialog = builder.create();
+			dialog.show();
+			return;
+		}
+		
+		try {
+			socket.send(packet);
+		} catch (IOException ioe) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(R.string.socket_error);
+			AlertDialog dialog = builder.create();
+			dialog.show();
+			return;
+		} finally {
+			socket.close();
+		}
+    }
+    
+    public void run(View view) {
+    	runOrStop(true);
+    }
+    
+    public void stop(View view) {
+    	runOrStop(false);
     }
 }
