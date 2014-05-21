8dfb498ebab88b76455b012ca31267967f9f85ff
Brandon Skari <brandon@skari.org>
2014-05-20 23:19:28 -0600

    Dump GPS data to the server

--- android/src/com/example/sparkfun_avc/MainActivity.java
+++ android/src/com/example/sparkfun_avc/MainActivity.java
@@ -1,17 +1,15 @@
 package com.example.sparkfun_avc;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
 import android.app.AlertDialog;
+import android.location.Criteria;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -23,9 +21,12 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
-	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-	
-	SensorDumpThread sensorDumpThread;
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    private SensorDumpThread sensorDumpThread = null;
+    private LocationManager locationManager = null;
+    private String bestProvider;
+    private DatagramSocket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +38,45 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.NO_REQUIREMENT);
+        criteria.setAltitudeRequired(false);
+        //criteria.setBearingAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setBearingRequired(true);
+        //criteria.setSpeedAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setSpeedRequired(true);
+        bestProvider = locationManager.getBestProvider(criteria, true);
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (sensorDumpThread.isAlive()) {
+            sensorDumpThread.stopDumping();
+        }
+
+        try {
+            sensorDumpThread.join();
+        } catch (InterruptedException e) {
+            // TODO: Figure out what to do here
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -75,120 +109,123 @@ public class MainActivity extends ActionBarActivity {
             return rootView;
         }
     }
-    
+
     private InetAddress getAddress() throws UnknownHostException {
-    	EditText editText = (EditText) findViewById(R.id.edit_server_ip);
-    	final String server = editText.getText().toString();
-    	return InetAddress.getByName(server);
+        EditText editText = (EditText) findViewById(R.id.edit_server_ip);
+        final String server = editText.getText().toString();
+        return InetAddress.getByName(server);
     }
-    
+
     public void sendSensorData(View view) {
-    	if (sensorDumpThread == null || !sensorDumpThread.isAlive()) {
-    		InetAddress address;
-    		try {
-    			address = getAddress();
-    		} catch (UnknownHostException uhe) {
-    			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    			builder.setMessage(R.string.server_not_found);
-    			AlertDialog dialog = builder.create();
-    			dialog.show();
-    			return;
-    		}
-    		
-    		byte[] buffer = "{\"requestResponse\": true}".getBytes();
-    		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 8384);
-    		DatagramSocket socket;
-			try {
-				socket = new DatagramSocket();
-			} catch (SocketException se) {
-    			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    			builder.setMessage(R.string.create_socket_error);
-    			AlertDialog dialog = builder.create();
-    			dialog.show();
-    			return;
-			}
-    		
-			try {
-				socket.send(packet);
-			} catch (IOException ioe) {
-    			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    			builder.setMessage(R.string.socket_error);
-    			AlertDialog dialog = builder.create();
-    			dialog.show();
-    			return;
-			} finally {
-				socket.close();
-			}
-			
-			// TODO: Wait for a response before making this thread
-			
-    		//sensorDumpThread = new SensorDumpThread(socket);
-    	}
+        if (sensorDumpThread == null || !sensorDumpThread.isAlive()) {
+            InetAddress address;
+            try {
+                address = getAddress();
+            } catch (UnknownHostException uhe) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.server_not_found);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return;
+            }
+
+            byte[] buffer = "{\"requestResponse\": true}".getBytes();
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 8384);
+            try {
+                if (socket == null || socket.isClosed()) {
+                    socket = new DatagramSocket();
+                }
+            } catch (SocketException se) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.create_socket_error);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return;
+            }
+
+            try {
+                socket.send(packet);
+            } catch (IOException ioe) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.socket_error);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return;
+            }
+
+            // TODO: Wait for a response before making this thread
+
+            sensorDumpThread = new SensorDumpThread(
+                    socket,
+                    locationManager,
+                    bestProvider,
+                    address,
+                    8384,
+                    100
+            );
+            sensorDumpThread.start();
+        }
     }
-    
+
     public void stopSensorData(View view) {
-    	if (sensorDumpThread != null && sensorDumpThread.isAlive()) {
-    		sensorDumpThread.stopDumping();
-    	}
+        if (sensorDumpThread != null && sensorDumpThread.isAlive()) {
+            sensorDumpThread.stopDumping();
+        }
     }
-    
+
     private void runOrStop(final boolean run) {
-    	if (run && (sensorDumpThread == null || !sensorDumpThread.isAlive())) {
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setMessage(R.string.not_running);
-			AlertDialog dialog = builder.create();
-			dialog.show();
-			return;
-    	}
-    	
-		InetAddress address;
-		try {
-			address = getAddress();
-		} catch (UnknownHostException uhe) {
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setMessage(R.string.server_not_found);
-			AlertDialog dialog = builder.create();
-			dialog.show();
-			return;
-		}
-		
-		byte[] buffer = String.format(
-			"{\"requestResponse\": true," +
-					" \"type\": \"command\"," + 
-					" \"command\": \"%s\"}",
-			run ? "run" : "stop"
-		).getBytes();
-		
-		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 8384);
-		DatagramSocket socket;
-		try {
-			socket = new DatagramSocket();
-		} catch (SocketException se) {
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setMessage(R.string.create_socket_error);
-			AlertDialog dialog = builder.create();
-			dialog.show();
-			return;
-		}
-		
-		try {
-			socket.send(packet);
-		} catch (IOException ioe) {
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setMessage(R.string.socket_error);
-			AlertDialog dialog = builder.create();
-			dialog.show();
-			return;
-		} finally {
-			socket.close();
-		}
+        if (run && (sensorDumpThread == null || !sensorDumpThread.isAlive())) {
+            alert(getString(R.string.not_running));
+            return;
+        }
+
+        InetAddress address;
+        try {
+            address = getAddress();
+        } catch (UnknownHostException uhe) {
+            alert(getString(R.string.server_not_found));
+            return;
+        }
+
+        byte[] buffer = String.format(
+            "{\"requestResponse\": true," +
+                    " \"type\": \"command\"," +
+                    " \"command\": \"%s\"}",
+            run ? "run" : "stop"
+        ).getBytes();
+
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 8384);
+        DatagramSocket socket;
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException se) {
+            alert(getString(R.string.create_socket_error));
+            return;
+        }
+
+        try {
+            socket.send(packet);
+        } catch (IOException ioe) {
+            alert(getString(R.string.socket_error));
+            return;
+        } finally {
+            socket.close();
+        }
     }
-    
+
     public void run(View view) {
-    	runOrStop(true);
+        runOrStop(true);
     }
-    
+
     public void stop(View view) {
-    	runOrStop(false);
+        runOrStop(false);
+    }
+
+    private void alert(final String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        return;
     }
 }
--- android/src/com/example/sparkfun_avc/SensorDumpThread.java
+++ android/src/com/example/sparkfun_avc/SensorDumpThread.java
@@ -1,45 +1,126 @@
 package com.example.sparkfun_avc;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 
 public class SensorDumpThread extends Thread {
-	DatagramSocket datagramSocket;
-	boolean running;
-	int frequencyMilliseconds;
-	
-	public SensorDumpThread(DatagramSocket datagramSocket) {
-		super("SensorDumpThread");
-		this.datagramSocket = datagramSocket;
-		this.running = true;
-		this.frequencyMilliseconds = 100;
-	}
-	
-	public SensorDumpThread(
-		DatagramSocket datagramSocket,
-		final int frequencyMilliseconds
-	) {
-		super("SensorDumpThread");
-		this.datagramSocket = datagramSocket;
-		this.running = true;
-		this.frequencyMilliseconds = frequencyMilliseconds;
-	}
-	
-	public void stopDumping() {
-		this.running = false;
-	}
-	
-	@Override
-	public void run() {
-		while (!datagramSocket.isClosed() && running) {
-			// Read from the sensors
-			// Send the sensor data
-			
-			try {
-				Thread.sleep(this.frequencyMilliseconds);
-			} catch (InterruptedException ie) {
-				// Ignore
-			}
-		}
-	}
+    DatagramSocket datagramSocket;
+    boolean running;
+    final int frequencyMilliseconds;
+    LocationManager manager;
+    final String bestProvider;
+    final InetAddress address;
+    final int port;
+    final MyLocationListener listener;
 
+    public SensorDumpThread(
+            DatagramSocket datagramSocket,
+            LocationManager manager,
+            String bestProvider,
+            final InetAddress address,
+            final int port
+    ) {
+        this(datagramSocket, manager, bestProvider, address, port, 100);
+    }
+
+    public SensorDumpThread(
+            DatagramSocket datagramSocket,
+            LocationManager manager,
+            String bestProvider,
+            final InetAddress address,
+            final int port,
+            final int frequencyMilliseconds
+    ) {
+        super("SensorDumpThread");
+        this.datagramSocket = datagramSocket;
+        this.manager = manager;
+        this.bestProvider = bestProvider;
+        this.address = address;
+        this.port = port;
+        this.frequencyMilliseconds = frequencyMilliseconds;
+
+        this.running = true;
+
+        listener = new MyLocationListener();
+        this.manager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                frequencyMilliseconds,
+                0,
+                listener
+        );
+    }
+
+    public void stopDumping() {
+        this.running = false;
+    }
+
+    @Override
+    public void run() {
+        while (!datagramSocket.isClosed() && running) {
+            final Location location = listener.location;
+
+            String json;
+            if (location != null) {
+                try {
+                    final double latitude = location.getLatitude();
+                    final double longitude = location.getLongitude();
+                    final float bearing = location.getBearing();
+                    json = String.format("{" +
+                            "\"type\": \"telemetry\"," +
+                            "\"latitude\": %4.9f," +
+                            "\"longitude\": %4.9f," +
+                            "\"bearing\": %4.9f" +
+                            "}",
+                            latitude,
+                            longitude,
+                            bearing
+                    );
+                } catch (Exception e) {
+                    json = e.toString();
+                }
+
+                byte[] buffer = json.getBytes();
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
+                try {
+                    datagramSocket.send(packet);
+                } catch (IOException e) {
+                    // Ignore
+                }
+            }
+
+            try {
+                Thread.sleep(this.frequencyMilliseconds);
+            } catch (InterruptedException ie) {
+                // Ignore
+            }
+        }
+    };
+
+    protected class MyLocationListener implements LocationListener {
+        public Location location;
+
+        @Override
+        public void onLocationChanged(Location location) {
+            this.location = location;
+        }
+
+        @Override
+        public void onProviderEnabled(final String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(final String provider) {
+        }
+
+        @Override
+        public void onStatusChanged(final String provider, int status, Bundle extras) {
+        }
+    };
 }
