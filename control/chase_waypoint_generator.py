9e385ee66269540f09217d7775ae0631658d6202
Brandon Skari <brandon@skari.org>
2015-01-03 20:56:38 -0700

    Start working on chase waypoints

---
+++ control/chase_waypoint_generator.py
@@ -0,0 +1,112 @@
+"""Implements the WaypointGenerator interface. Returns waypoints from a KML
+file. All WaypointGenerator implementations should have two methods:
+    get_current_waypoint(self, latitude, longitude) -> (float, float)
+    reached(self, latitude, longitude) -> bool
+    next(self)
+    done(self) -> bool
+This implements the "rabbit chase" algorithm.
+"""
+
+import math
+
+from kml_waypoint_generator import KmlWaypointGenerator
+from telemetry import Telemetry
+
+
+class ChaseWaypointGenerator(object):
+    """Loads waypoints from a KML file and returns waypoints using the 'rabbit
+    chase' algorithm.
+    """
+
+    def __init__(self, logger, kml_file_name, distance_m=None):
+        self._logger = logger
+
+        if distance_m is None:
+            self._distance_m = 2.0
+        else:
+            self._distance_m = distance_m
+
+        kml_waypoint_generator = KmlWaypointGenerator(kml_file_name)
+        self._waypoints = list(kml_waypoint_generator._waypoints)  # pylint: disable=protected-access
+        self._current_waypoint = 0
+
+        self._last_latitude = None
+        self._last_longitude = None
+
+    def get_current_waypoint(self):
+        """Returns the current waypoint."""
+        if self._current_waypoint == 0:
+            return self._waypoints[self._current_waypoint]
+        elif len(self._waypoints) == 1:
+            return self._waypoints[self._current_waypoint]
+        else:
+            distance_m = Telemetry.distance_m(
+                self._waypoints[self._current_waypoint][0],
+                self._waypoints[self._current_waypoint][1],
+                self._last_latitude,
+                self._last_longitude
+            )
+            if distance_m < self._distance_m:
+                return self._waypoints[self._current_waypoint]
+
+            # Find the point on the line segment from the previous waypoint to
+            # the current waypoint that is self._distance_m away and that's in
+            # the direction of the next waypoint
+
+        raise ValueError('No waypoints left')
+
+    @staticmethod
+    def _circle_intersection(point1, point2, circle_center, circle_radius):
+        """Returns an iterable list of the points of intersection between a line
+        and a circle, if any.
+        """
+        # http://mathworld.wolfram.com/Circle-LineIntersection.html
+        x_1 = point1[0] - circle_center[0]
+        x_2 = point2[0] - circle_center[0]
+        y_1 = point1[1] - circle_center[1]
+        y_2 = point2[1] - circle_center[1]
+        d_x = x_2 - x_1
+        d_y = y_2 - y_1
+        d_r = math.sqrt(d_x ** 2 + d_y ** 2)
+        determinant = x_1 * y_2 - x_2 * y_1
+
+        discriminant = circle_radius ** 2 * d_r ** 2 - determinant ** 2
+        if discriminant < 0:
+            return ()
+
+        root = math.sqrt(discriminant)
+        d_r_2 = d_r ** 2
+        x_1 = (determinant * d_y + d_x * root) / d_r_2
+        x_2 = (determinant * d_y - d_x * root) / d_r_2
+        y_1 = (-determinant * d_x + abs(d_y) * root) / d_r_2
+        y_2 = (-determinant * d_x - abs(d_y) * root) / d_r_2
+
+        # Degenerate case of a tangent line
+        if x_1 == x_2 and y_1 == y_2:
+            return ((x_1 + circle_center[0], y_1 + circle_center[1]),)
+
+        return (
+            (x_1 + circle_center[0], y_1 + circle_center[1]),
+            (x_2 + circle_center[0], y_2 + circle_center[1])
+        )
+
+    def reached(self, latitude, longitude):
+        """Returns True if the current waypoint has been reached."""
+        self._last_latitude = latitude
+        self._last_longitude = longitude
+        return Telemetry.distance_m(
+            latitude,
+            longitude,
+            self._waypoints[0][0],
+            self._waypoints[0][1]
+        ) < 1.5
+
+    def next(self):
+        """Goes to the next waypoint."""
+        self._current_waypoint += 1
+
+    def done(self):
+        """Returns True if the course is done and there are no remaining
+        waypoints.
+        """
+        return self._current_waypoint == len(self._waypoints)
---
+++ control/test/test_chase_waypoint_generator.py
@@ -0,0 +1,116 @@
+"""Tests the Telemetry class."""
+import math
+import unittest
+
+from chase_waypoint_generator import ChaseWaypointGenerator
+
+#pylint: disable=invalid-name
+#pylint: disable=protected-access
+#pylint: disable=too-many-public-methods
+
+
+class TestChaseWaypointGenerator(unittest.TestCase):
+    """Tests the ChaseWaypointGenerator class."""
+
+    def test_circle_intersection_none(self):
+        """Tests circle line intersection none case."""
+        intersections = ChaseWaypointGenerator._circle_intersection(
+            (-5, 0),
+            (5, 0),
+            (0, -10),
+            9
+        )
+        self.assertEqual(len(intersections), 0)
+
+    def test_circle_intersection_degenerate(self):
+        """Tests circle line intersection degenerate case."""
+
+
+    def test_circle_intersection_dual(self):
+        """Tests circle line intersection two points case."""
+
+        def _almost_equal_set_of_points(list_1, list_2, offset):
+            """Tests a set of points for approximate equality."""
+            for point_1 in list_1:
+                matched = False
+                for point_2 in list_2:
+                    point_2 = self._add(point_2, offset)
+                    if (
+                        abs(point_1[0] - point_2[0]) < 0.00001
+                        and abs(point_1[1] - point_2[1]) < 0.00001
+                    ):
+                        matched = True
+                        break
+                if not matched:
+                    return False
+            return True
+
+        for p_1, p_2, circle, radius, expected in (
+            # Vertical
+            ((0, -10), (0, 10), (0, 0), 1, ((0, -1), (0, 1))),
+            # Horizontal
+            ((-3, 0), (300, 0), (0, 0), 2, ((2, 0), (-2, 0))),
+        ):
+            for offset in (
+                (0, 0),
+                (5, 0),
+                (-6, 0),
+                (0, 7),
+                (0, -8),
+                (1, 1),
+                (1, -1),
+                (-1, 1),
+                (-1, -1),
+            ):
+                intersections = ChaseWaypointGenerator._circle_intersection(
+                    self._add(p_1, offset),
+                    self._add(p_2, offset),
+                    self._add(circle, offset),
+                    radius
+                )
+                self.assertEqual(len(intersections), 2)
+                self.assertTrue(
+                    _almost_equal_set_of_points(
+                        intersections,
+                        expected,
+                        offset
+                    ),
+                    'Calculated {}, expected {}'.format(intersections, expected)
+                )
+
+                intersections = ChaseWaypointGenerator._circle_intersection(
+                    self._add(p_1, offset),
+                    self._add(p_2, offset),
+                    self._add(circle, offset),
+                    radius
+                )
+                self.assertEqual(len(intersections), 2)
+                self.assertTrue(
+                    _almost_equal_set_of_points(
+                        intersections,
+                        expected,
+                        offset
+                    ),
+                    'Calculated {}, expected {}'.format(intersections, expected)
+                )
+
+        # Diagonal
+        # This case is degenerate itself, because it relies on floating point
+        p_1, p_2, circle, radius, expected = (
+            (-2, 0), (0, 2), (0, 0), math.sqrt(2), (-1, 1)
+        )
+        intersections = ChaseWaypointGenerator._circle_intersection(
+            p_1,
+            p_2,
+            circle,
+            radius
+        )
+        self.assertGreaterEqual(len(intersections), 1)
+        for intersection in intersections:
+            for value, expected_value in zip(intersection, expected):
+                self.assertAlmostEqual(value, expected_value)
+
+    @staticmethod
+    def _add(point_1, point_2):
+        """Adds two points."""
+        return (point_1[0] + point_2[0], point_1[1] + point_2[1])
