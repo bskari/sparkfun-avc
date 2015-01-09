1b51216ae73430fbe70a8db566d3a074768befa4
Brandon Skari <brandon@skari.org>
2015-01-10 06:46:51 +0700

    Change directory to the script when running rooter

--- setup/rooter.c
+++ setup/rooter.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+
+#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 
 int main(int argc, char* argv[]) {
     if (argc <= 1) {
@@ -7,5 +10,18 @@ int main(int argc, char* argv[]) {
         return -1;
     }
     argv[0] = "python";
+    char path_buffer[512];
+    strncpy(path_buffer, argv[1], COUNT_OF(path_buffer));
+    int separator_index;
+    for (separator_index = COUNT_OF(path_buffer) - 1; separator_index >= 0; --separator_index) {
+        if (path_buffer[separator_index] == '/') {
+            path_buffer[separator_index] = '\0';
+            break;
+        }
+    }
+    int success = chdir(path_buffer);
+    if (success != 0) {
+        return success;
+    }
     return execv("/home/pi/.virtualenvs/sparkfun/bin/python", argv);
 }
