072945c3f5038a5e81fc85f33a56048ca5a2df38
Brandon Skari <brandon@skari.org>
2015-01-30 13:03:14 +0700

    Fix sine calculation and start work on arc tan function.

--- control-rust/src/telemetry.rs
+++ control-rust/src/telemetry.rs
@@ -1,4 +1,5 @@
 use std::f32;
+use std::iter::range_step;
 
 use telemetry_message::TelemetryMessage;
 
@@ -39,10 +40,10 @@ pub trait Telemetry {
 pub fn rotate_degrees_clockwise(point:(f32, f32), degrees:f32) -> (f32, f32) {
     let (pt_x, pt_y) = point;
     // So, sine and cosine appear to be messed up on Rust 0.13 on Raspberry Pi.
-    // sin just returns the value
+    // sin just returns the value, so we have to use our own implmenetation.
     let sine = degrees.sine_d();
     let cosine = degrees.cosine_d();
-    (pt_x * cosine - pt_y * sine, pt_x * sine + pt_y * cosine)
+    (cosine * pt_x + sine * pt_y, -sine * pt_x + cosine * pt_y)
 }
 
 
@@ -79,8 +80,8 @@ pub fn latitude_d_to_m_per_longitude_d(latitude_d: f32) -> f32 {
  * left to reach a goal heading in degrees.
  */
 pub fn is_turn_left(heading_d: f32, goal_heading_d: f32) -> bool {
-    let (pt_1_0, pt_1_1) = rotate_degrees_clockwise((1.0f32, 0.0f32), heading_d);
-    let (pt_2_0, pt_2_1) = rotate_degrees_clockwise((1.0f32, 0.0f32), goal_heading_d);
+    let (pt_1_0, pt_1_1) = rotate_degrees_clockwise((0.0f32, 1.0f32), heading_d);
+    let (pt_2_0, pt_2_1) = rotate_degrees_clockwise((0.0f32, 1.0f32), goal_heading_d);
     let cross_product = pt_1_0 * pt_2_1 - pt_1_1 * pt_2_0;
     if cross_product > 0.0 {
         return true;
@@ -103,7 +104,7 @@ pub fn relative_degrees(x_1: f32, y_1: f32, x_2: f32, y_2: f32) -> f32 {
         return 180.0;
     }
 
-    let degrees = (relative_y_m / relative_x_m).atan().to_degrees();
+    let degrees = (relative_y_m / relative_x_m).arc_tan();
     if relative_x_m > 0.0 {
         return 90.0 - degrees;
     } else {
@@ -152,35 +153,73 @@ trait MyTrig {
     fn pow_i(&self, exponent: i32) -> f32;
     fn sine_d(&self) -> f32;
     fn cosine_d(&self) -> f32;
+    fn arc_tan(&self) -> f32;
 }
 impl MyTrig for f32 {
     fn pow_i(&self, exponent: i32) -> f32 {
         // TODO: Maybe do a divide and conquer if we use any large exponents
         let mut value = *self;
-        for i in range(1, exponent) {
+        for _ in range(1, exponent) {
             value *= *self;
         }
         return value;
     }
     fn sine_d(&self) -> f32 {
         let mut degrees = wrap_degrees(*self);
-        let pi = 3.14159265358979323846264338327950288419;
-        if degrees > 270.0 {
-            degrees = 360.0 - degrees;
-        } else if degrees > 90.0 {
+        if degrees > 180.0 {
             degrees = 180.0 - degrees;
         }
+
+        // TODO: sine is cyclic every 45 degrees, so we could reduce
+        // the required computations even more here by playing tricks
+        if degrees > 90.0 {
+            degrees = 180.0 - degrees;
+        } else if degrees < -90.0 {
+            degrees = -180.0 - degrees;
+        }
+        let pi: f32 = 3.14159265358979323846264338327950288419;
         let radians: f32 = degrees * pi / 180.0;
-        return radians
+        radians
             - radians.pow_i(3) / 6f32 // factorial(3)
             + radians.pow_i(5) / 120f32 // factorial(5)
             - radians.pow_i(7) / 5040f32 // factorial(7)
             + radians.pow_i(9) / 362880f32 // factorial(9)
-        ;
     }
 
     fn cosine_d(&self) -> f32 {
         (90.0 - *self).sine_d()
+    }
+
+    fn arc_tan(&self) -> f32 {
+        let pi: f32 = 3.14159265358979323846264338327950288419;
+
+        return *self
+            - 1.0 / (3.0 / *self)
+            + 1.0 / (5.0 / (*self).pow_i(5))
+            - 1.0 / (7.0 / (*self).pow_i(7))
+            ;
+
+        /*
+        if target < 1.0 {
+            value = target
+                + target.pow_i(3) / 3f32
+                - 2.0 * target.pow_i(5) / 15f32
+                + 17.0 * target.pow_i(7) / 315f32
+                ;
+        } else {
+            value = 0.5 * pi
+                - 1.0 /target 
+                + 1.0 / (3.0 * target.pow_i(3))
+                - 1.0 / (5.0 * target.pow_i(5))
+                + 1.0 / (7.0 * target.pow_i(7))
+                ;
+        }
+        if negative {
+            -value * 180.0 / pi
+        } else {
+            value * 180.0 / pi
+        }
+        */
     }
 }
 #[test]
@@ -197,8 +236,9 @@ fn test_sine_d() {
     assert!((360.0f32.sine_d() - 0.0f32).abs() < 0.00001f32);
     assert!((720.0f32.sine_d() - 0.0f32).abs() < 0.00001f32);
     assert!((1080.0f32.sine_d() - 0.0f32).abs() < 0.00001f32);
+    assert!((-45.0f32.sine_d() - -0.70711f32).abs() < 0.00001f32);
     assert!((-90.0f32.sine_d() - -1.0f32).abs() < 0.00001f32);
-    assert!((-180.0f32.sine_d() - 0.0f32).abs() < 0.00001f32);
+    assert!((-180.0f32.sine_d() - 0.0f32).abs() < 0.0001f32);
     assert!((-270.0f32.sine_d() - 1.0f32).abs() < 0.00001f32);
     assert!((-360.0f32.sine_d() - 0.0f32).abs() < 0.00001f32);
     assert!((-720.0f32.sine_d() - 0.0f32).abs() < 0.00001f32);
@@ -212,10 +252,22 @@ fn test_cosine_d() {
     assert!((180.0f32.cosine_d() - -1.0f32).abs() < 0.00001f32);
     assert!((270.0f32.cosine_d() - 0.0f32).abs() < 0.00001f32);
     assert!((360.0f32.cosine_d() - 1.0f32).abs() < 0.00001f32);
-    assert!((-90.0f32.cosine_d() - 0.0f32).abs() < 0.00001f32);
-    assert!((-180.0f32.cosine_d() - -1.0f32).abs() < 0.00001f32);
-    assert!((-270.0f32.cosine_d() - 0.0f32).abs() < 0.00001f32);
-    assert!((-360.0f32.cosine_d() - 1.0f32).abs() < 0.00001f32);
+    assert!(((-90.0f32).cosine_d() - 0.0f32).abs() < 0.00001f32);
+    assert!(((-180.0f32).cosine_d() - -1.0f32).abs() < 0.00001f32);
+    assert!(((-270.0f32).cosine_d() - 0.0f32).abs() < 0.00001f32);
+    assert!(((-360.0f32).cosine_d() - 1.0f32).abs() < 0.00001f32);
+}
+#[test]
+fn test_arc_tan() {
+    // Expected values computed in Python from range_step(-5, 5.1, 0.1)
+    let values: Vec<f32> = vec![-5.00, -4.90, -4.80, -4.70, -4.60, -4.50, -4.40, -4.30, -4.20, -4.10, -4.00, -3.90, -3.80, -3.70, -3.60, -3.50, -3.40, -3.30, -3.20, -3.10, -3.00, -2.90, -2.80, -2.70, -2.60, -2.50, -2.40, -2.30, -2.20, -2.10, -2.00, -1.90, -1.80, -1.70, -1.60, -1.50, -1.40, -1.30, -1.20, -1.10, -1.00, -0.90, -0.80, -0.70, -0.60, -0.50, -0.40, -0.30, -0.20, -0.10, 0.00, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.00, 1.10, 1.20, 1.30, 1.40, 1.50, 1.60, 1.70, 1.80, 1.90, 2.00, 2.10, 2.20, 2.30, 2.40, 2.50, 2.60, 2.70, 2.80, 2.90, 3.00, 3.10, 3.20, 3.30, 3.40, 3.50, 3.60, 3.70, 3.80, 3.90, 4.00, 4.10, 4.20, 4.30, 4.40, 4.50, 4.60, 4.70, 4.80, 4.90, 5.00];
+    let expected_d: Vec<f32> = vec![-78.69f32, -78.47, -78.23, -77.99, -77.74, -77.47, -77.20, -76.91, -76.61, -76.29, -75.96, -75.62, -75.26, -74.88, -74.48, -74.05, -73.61, -73.14, -72.65, -72.12, -71.57, -70.97, -70.35, -69.68, -68.96, -68.20, -67.38, -66.50, -65.56, -64.54, -63.43, -62.24, -60.95, -59.53, -57.99, -56.31, -54.46, -52.43, -50.19, -47.73, -45.00, -41.99, -38.66, -34.99, -30.96, -26.57, -21.80, -16.70, -11.31, -5.71, 0.00, 5.71, 11.31, 16.70, 21.80, 26.57, 30.96, 34.99, 38.66, 41.99, 45.00, 47.73, 50.19, 52.43, 54.46, 56.31, 57.99, 59.53, 60.95, 62.24, 63.43, 64.54, 65.56, 66.50, 67.38, 68.20, 68.96, 69.68, 70.35, 70.97, 71.57, 72.12, 72.65, 73.14, 73.61, 74.05, 74.48, 74.88, 75.26, 75.62, 75.96, 76.29, 76.61, 76.91, 77.20, 77.47, 77.74, 77.99, 78.23, 78.47, 78.69];
+    for index in range(0u32, values.len() as u32) {
+        let value = values[index as uint].arc_tan();
+        let expected = expected_d[index as uint];
+        println!("value {}, expected {}, diff {}", value, expected, (value - expected).abs());
+    }
+    assert!(false);
 }
 
 
@@ -232,13 +284,16 @@ mod tests {
 
 
     fn assert_approx_eq(value_1:f32, value_2:f32) -> () {
+        assert!(approx_eq(value_1, value_2));
+    }
+    fn approx_eq(value_1:f32, value_2:f32) -> bool {
         // Yeah, I know this is bad, see
         // http://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
 
         // This is the best we can do with f32
         let tolerance: f32 = 0.00001;
         let diff = (value_1 - value_2).abs();
-        assert!(diff < tolerance, "{} < {} failed", diff, tolerance);
+        diff < tolerance
     }
 
 
@@ -256,58 +311,41 @@ mod tests {
         let base_y = 1.0f32;
         let base = (base_x, base_y);
 
-        println!("{}", "(base, 0.0, base)");
         test_rotate(base, 0.0, base);
-        println!("{}", "(base, 90.0, (base_y, -base_x))");
+        test_rotate(base, 45.0, (0.707106781f32, 0.707106781f32));
         test_rotate(base, 90.0, (base_y, -base_x));
-        println!("{}", "(base, 180.0, (-base_x, base_y))");
-        test_rotate(base, 180.0, (-base_x, base_y));
-        println!("{}", "(base, 270.0, (base_y, base_x))");
-        test_rotate(base, 270.0, (base_y, base_x));
-        println!("{}", "(base, 360.0, base)");
+        test_rotate(base, 180.0, (-base_x, -base_y));
+        test_rotate(base, 270.0, (-base_y, base_x));
         test_rotate(base, 360.0, base);
 
-        println!("{}", "(base, -90.0, (base_y, base_x))");
-        test_rotate(base, -90.0, (base_y, base_x));
-        println!("{}", "(base, -180.0, (-base_x, base_y))");
-        test_rotate(base, -180.0, (-base_x, base_y));
-        println!("{}", "(base, -270.0, (base_y, -base_x))");
-        test_rotate(base, -270.0, (base_y, -base_x));
-        println!("{}", "(base, -360.0, base)");
+        let (x_359, y_359) = rotate_degrees_clockwise(base, 359.0);
+        assert!(-0.9 < x_359 && x_359 < base_x);
+        assert!(y_359 > base_y && base_y > 0.9);
+
+        test_rotate(base, -90.0, (-base_y, base_x));
+        test_rotate(base, -180.0, (base_x, -base_y));
+        test_rotate(base, -270.0, (base_y, base_x));
         test_rotate(base, -360.0, base);
 
-        println!("{}", "(base, 720.0, base)");
         test_rotate(base, 720.0, base);
-        println!("{}", "(base, -720.0, base)");
         test_rotate(base, -720.0, base);
 
         let skewed_x = 1.0;
         let skewed_y = 2.0;
         let skewed = (skewed_x, skewed_y);
 
-        println!("{}", "(skewed, 0.0, skewed)");
         test_rotate(skewed, 0.0, skewed);
-        println!("{}", "(skewed, 90.0, (skewed_y, -skewed_x))");
         test_rotate(skewed, 90.0, (skewed_y, -skewed_x));
-        println!("{}", "(skewed, 180.0, (-skewed_x, -skewed_y))");
         test_rotate(skewed, 180.0, (-skewed_x, -skewed_y));
-        println!("{}", "(skewed, 270.0, (-skewed_y, skewed_x))");
         test_rotate(skewed, 270.0, (-skewed_y, skewed_x));
-        println!("{}", "(skewed, 360.0, skewed)");
         test_rotate(skewed, 360.0, skewed);
 
-        println!("{}", "(skewed, -90.0, (-skewed_y, skewed_x))");
         test_rotate(skewed, -90.0, (-skewed_y, skewed_x));
-        println!("{}", "(skewed, -180.0, (-skewed_x, -skewed_y))");
         test_rotate(skewed, -180.0, (-skewed_x, -skewed_y));
-        println!("{}", "(skewed, -270.0, (skewed_y, -skewed_x))");
         test_rotate(skewed, -270.0, (skewed_y, -skewed_x));
-        println!("{}", "(skewed, -360.0, skewed)");
         test_rotate(skewed, -360.0, skewed);
 
-        println!("{}", "(skewed, 720.0, skewed)");
         test_rotate(skewed, 720.0, skewed);
-        println!("{}", "(skewed, -720.0, skewed)");
         test_rotate(skewed, -720.0, skewed);
     }
 
@@ -363,24 +401,40 @@ mod tests {
 
     #[test]
     fn test_relative_degrees() {
+        println!("{}", relative_degrees(0.0, 0.0, 1.0, 1.0));
         assert_approx_eq(relative_degrees(0.0, 0.0, 1.0, 1.0), 45.0);
+        println!("{}", relative_degrees(1.0, 1.0, 0.0, 0.0));
         assert_approx_eq(relative_degrees(1.0, 1.0, 0.0, 0.0), 225.0);
+        println!("{}", relative_degrees(0.0, 0.0, 2.0, 2.0));
         assert_approx_eq(relative_degrees(0.0, 0.0, 2.0, 2.0), 45.0);
+        println!("{}", relative_degrees(2.0, 2.0, 0.0, 0.0));
         assert_approx_eq(relative_degrees(2.0, 2.0, 0.0, 0.0), 225.0);
 
+        println!("{}", relative_degrees(0.0, 0.0, -1.0, 1.0));
         assert_approx_eq(relative_degrees(0.0, 0.0, -1.0, 1.0), 315.0);
+        println!("{}", relative_degrees(-1.0, 1.0, 0.0, 0.0));
         assert_approx_eq(relative_degrees(-1.0, 1.0, 0.0, 0.0), 135.0);
+        println!("{}", relative_degrees(0.0, 0.0, -2.0, 2.0));
         assert_approx_eq(relative_degrees(0.0, 0.0, -2.0, 2.0), 315.0);
+        println!("{}", relative_degrees(-2.0, 2.0, 0.0, 0.0));
         assert_approx_eq(relative_degrees(-2.0, 2.0, 0.0, 0.0), 135.0);
 
+        println!("{}", relative_degrees(0.0, 0.0, 0.0, 1.0));
         assert_approx_eq(relative_degrees(0.0, 0.0, 0.0, 1.0), 0.0);
+        println!("{}", relative_degrees(0.0, 1.0, 0.0, 0.0));
         assert_approx_eq(relative_degrees(0.0, 1.0, 0.0, 0.0), 180.0);
+        println!("{}", relative_degrees(0.0, 0.0, 0.0, 2.0));
         assert_approx_eq(relative_degrees(0.0, 0.0, 0.0, 2.0), 0.0);
+        println!("{}", relative_degrees(0.0, 2.0, 0.0, 0.0));
         assert_approx_eq(relative_degrees(0.0, 2.0, 0.0, 0.0), 180.0);
 
+        println!("{}", relative_degrees(0.0, 0.0, 1.0, 0.0));
         assert_approx_eq(relative_degrees(0.0, 0.0, 1.0, 0.0), 90.0);
+        println!("{}", relative_degrees(1.0, 0.0, 0.0, 0.0));
         assert_approx_eq(relative_degrees(1.0, 0.0, 0.0, 0.0), 270.0);
+        println!("{}", relative_degrees(0.0, 0.0, 2.0, 0.0));
         assert_approx_eq(relative_degrees(0.0, 0.0, 2.0, 0.0), 90.0);
+        println!("{}", relative_degrees(2.0, 0.0, 0.0, 0.0));
         assert_approx_eq(relative_degrees(2.0, 0.0, 0.0, 0.0), 270.0);
     }
 
