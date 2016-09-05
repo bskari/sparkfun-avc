a8c113271b58e3b6b9e2986d80a5838ffb9ab4b8
Brandon Skari <brandon@skari.org>
2016-09-04 22:18:18 -0600

    Add color paths

--- analysis/process_gps.py
+++ analysis/process_gps.py
@@ -7,9 +7,15 @@ import sys
 
 
 KML_TEMPLATE = None
+COLOR_MAP = {
+    # Default the estimate to yellow
+    # ABGR
+    'estimate': 'ff00ffff'
+}
 
 PLACEMARK_TEMPLATE = '''<Placemark>
     <name>{name}</name>
+    <styleUrl>{style_url}</styleUrl>
     <LineString>
             <tessellate>1</tessellate>
             <coordinates>
@@ -18,6 +24,32 @@ PLACEMARK_TEMPLATE = '''<Placemark>
     </LineString>
 </Placemark>
 '''
+
+STYLE_TEMPLATE = '''<StyleMap id="m{id}">
+        <Pair>
+                <key>normal</key>
+                <styleUrl>#{id}0</styleUrl>
+        </Pair>
+        <Pair>
+                <key>highlight</key>
+                <styleUrl>#{id}1</styleUrl>
+        </Pair>
+</StyleMap>
+<Style id="{id}0">
+        <IconStyle>
+                <scale>{scale}</scale>
+        </IconStyle>
+        <LineStyle>
+                <color>{color}</color>
+                <width>1</width>
+        </LineStyle>
+</Style>
+<Style id="{id}1">
+        <LineStyle>
+                <color>{color}</color>
+                <width>1</width>
+        </LineStyle>
+</Style>'''
 
 FOLDER_TEMPLATE = '''<Folder>
     <name>{name}</name>
@@ -40,8 +72,28 @@ def main():
     date = in_file_name[in_file_name.find('-') + 1:in_file_name.rfind('.')]
     out_file_name = sys.argv[2] if len(sys.argv) > 2 else 'out.kml'
     with open(in_file_name) as in_stream:
+        build_color_map(in_stream)
+    with open(in_file_name) as in_stream:
         with open(out_file_name, 'w') as out_stream:
             process_streams(in_stream, out_stream, date)
+
+
+def build_color_map(in_stream):
+    global COLOR_MAP
+    # ABGR
+    preferred_colors = ['ff0000ff', 'ff00ff00', 'ff0000ff']
+    for line in in_stream:
+        if '"device_id"' in line:
+            parts = json.loads(line[line.find('{'):line.rfind('}') + 1])
+            device = device_id_to_style_url(parts['device_id'])
+            if device in COLOR_MAP:
+                continue
+
+            if len(preferred_colors) > 0:
+                color = preferred_colors.pop()
+            else:
+                color = 'ffaaffff'
+            COLOR_MAP[device] = color
 
 
 def process_streams(in_stream, out_stream, name):
@@ -56,7 +108,14 @@ def process_streams(in_stream, out_stream, name):
     out_stream.write(
         KML_TEMPLATE.format(
             name=name,
-            folders=''.join(runs)
+            style_maps='\n'.join((
+                STYLE_TEMPLATE.format(
+                    id=device_id_to_style_url(device),
+                    scale=1.2,
+                    color=color
+                ) for device, color in COLOR_MAP.items()
+            )),
+            folders=''.join(runs),
         )
     )
 
@@ -94,11 +153,16 @@ def process_run(in_stream, name, run_count):
                         longitude=point[1]
                     )
                     for point in points[device]
-                ))
+                )),
+                style_url='#m{id}'.format(id=device_id_to_style_url(device)),
             )
             for device in points
         ))
     )
+
+
+def device_id_to_style_url(device):
+    return device[:20].replace(' ', '-')
 
 
 if __name__ == '__main__':
