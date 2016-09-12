f585bf3c1cfbd8c3e5cd159561104849b607aa82
Brandon Skari <brandon@skari.org>
2016-09-11 21:08:02 -0600

    Factor out point formatting

---
+++ analysis/plot_points.py
@@ -0,0 +1,170 @@
+"""Writes a KML file of points from stdin."""
+
+import fileinput
+
+
+PLACEMARK_TEMPLATE = '''<Placemark>
+    <name>{name}</name>
+    <styleUrl>{style_url}</styleUrl>
+    <LineString>
+            <tessellate>1</tessellate>
+            <coordinates>
+                {coordinates}
+        </coordinates>
+    </LineString>
+</Placemark>
+'''
+
+STYLE_TEMPLATE = '''<StyleMap id="m{id}">
+        <Pair>
+                <key>normal</key>
+                <styleUrl>#{id}0</styleUrl>
+        </Pair>
+        <Pair>
+                <key>highlight</key>
+                <styleUrl>#{id}1</styleUrl>
+        </Pair>
+</StyleMap>
+<Style id="{id}0">
+        <IconStyle>
+                <scale>{scale}</scale>
+        </IconStyle>
+        <LineStyle>
+                <color>{color}</color>
+                <width>1</width>
+        </LineStyle>
+</Style>
+<Style id="{id}1">
+        <LineStyle>
+                <color>{color}</color>
+                <width>1</width>
+        </LineStyle>
+</Style>'''
+
+FOLDER_TEMPLATE = '''<Folder>
+    <name>{name}</name>
+    {placemark}
+</Folder>
+'''
+
+
+def build_color_map(runs):
+    """Returns the color map for a series of runs.
+
+    runs is:
+      [
+        {
+          'device_id_1': [(lat, long), (lat, long), ...],
+          'device_id_2': [(lat, long), (lat, long), ...]
+          ...
+        },
+        {
+          'device_id_1': [(lat, long), (lat, long), ...],
+          'device_id_2': [(lat, long), (lat, long), ...]
+          ...
+        },
+        ...
+    ]
+    """
+
+    color_map = {
+        # Default the estimate to yellow
+        # ABGR
+        'estimate': 'ff00ffff'
+    }
+    # ABGR
+    preferred_colors = ['ff0000ff', 'ff00ff00', 'ffff0000']
+    for run in runs:
+        for device in run:
+            if device in color_map:
+                continue
+
+            if len(preferred_colors) > 0:
+                color = preferred_colors.pop()
+            else:
+                color = 'ffaaffff'
+            color_map[device] = color
+    return color_map
+
+
+def device_id_to_style_url(device):
+    """Maps device id to a style URL."""
+    return device[:20].replace(' ', '-')
+
+
+def get_kml(runs, name):
+    """Returns the KML for a series of runs.
+
+    runs is:
+      [
+        {
+          'device_id_1': [(lat, long), (lat, long), ...],
+          'device_id_2': [(lat, long), (lat, long), ...]
+          ...
+        },
+        {
+          'device_id_1': [(lat, long), (lat, long), ...],
+          'device_id_2': [(lat, long), (lat, long), ...]
+          ...
+        },
+        ...
+    ]
+    """
+    folders = ''.join(
+        FOLDER_TEMPLATE.format(
+            name=str(run_count + 1),
+            placemark='\n'.join((
+                PLACEMARK_TEMPLATE.format(
+                    name=device,
+                    coordinates=' '.join((
+                        '{longitude},{latitude},0'.format(
+                            latitude=point[0],
+                            longitude=point[1]
+                        )
+                        for point in points[device]
+                    )),
+                    style_url='#m{id}'.format(id=device_id_to_style_url(device)),
+                )
+                for device in points
+            ))
+        )
+        for run_count, points in enumerate(runs)
+    )
+
+    with open('kml_template.xml') as file_:
+        kml_template = file_.read()
+
+    color_map = build_color_map(runs)
+
+    return kml_template.format(
+        name=name,
+        style_maps='\n'.join((
+            STYLE_TEMPLATE.format(
+                id=device_id_to_style_url(device),
+                scale=1.2,
+                color=color
+            ) for device, color in color_map.items()
+        )),
+        folders=''.join(folders),
+    )
+
+
+def main():
+    """Read points from stdin."""
+    print('Reading points from stdin, formatted as "lat,long" one per line')
+    points = []
+    for line in fileinput.input():
+        latitude, longitude = [float(i) for i in line.split(',')]
+        points.append((latitude, longitude))
+
+    runs = [
+        {
+            'stdin': points
+        }
+    ]
+    with open('stdin.kml', 'w') as out_file:
+        out_file.write(get_kml(runs, 'stdin'))
+
+
+if __name__ == '__main__':
+    main()
--- analysis/process_gps.py
+++ analysis/process_gps.py
@@ -5,57 +5,7 @@ import collections
 import json
 import sys
 
-
-KML_TEMPLATE = None
-COLOR_MAP = {
-    # Default the estimate to yellow
-    # ABGR
-    'estimate': 'ff00ffff'
-}
-
-PLACEMARK_TEMPLATE = '''<Placemark>
-    <name>{name}</name>
-    <styleUrl>{style_url}</styleUrl>
-    <LineString>
-            <tessellate>1</tessellate>
-            <coordinates>
-                {coordinates}
-        </coordinates>
-    </LineString>
-</Placemark>
-'''
-
-STYLE_TEMPLATE = '''<StyleMap id="m{id}">
-        <Pair>
-                <key>normal</key>
-                <styleUrl>#{id}0</styleUrl>
-        </Pair>
-        <Pair>
-                <key>highlight</key>
-                <styleUrl>#{id}1</styleUrl>
-        </Pair>
-</StyleMap>
-<Style id="{id}0">
-        <IconStyle>
-                <scale>{scale}</scale>
-        </IconStyle>
-        <LineStyle>
-                <color>{color}</color>
-                <width>1</width>
-        </LineStyle>
-</Style>
-<Style id="{id}1">
-        <LineStyle>
-                <color>{color}</color>
-                <width>1</width>
-        </LineStyle>
-</Style>'''
-
-FOLDER_TEMPLATE = '''<Folder>
-    <name>{name}</name>
-    {placemark}
-</Folder>
-'''
+from plot_points import get_kml
 
 
 def main():
@@ -64,63 +14,30 @@ def main():
         print('Usage: {} <log file>'.format(sys.argv[0]))
         return
 
-    global KML_TEMPLATE
-    with open('kml_template.xml') as file_:
-        KML_TEMPLATE = file_.read()
-
     in_file_name = sys.argv[1]
     name = in_file_name[:in_file_name.rfind('.')]
     out_file_name = sys.argv[2] if len(sys.argv) > 2 else 'out.kml'
     with open(in_file_name) as in_stream:
-        build_color_map(in_stream)
-    with open(in_file_name) as in_stream:
-        with open(out_file_name, 'w') as out_stream:
-            process_streams(in_stream, out_stream, name)
+        lines = in_stream.readlines()
+    runs = process_lines(iter(lines))
+    with open(out_file_name, 'w') as out_stream:
+        out_stream.write(get_kml(runs, name))
 
 
-def build_color_map(in_stream):
-    global COLOR_MAP
-    # ABGR
-    preferred_colors = ['ff0000ff', 'ff00ff00', 'ffff0000']
-    for line in in_stream:
-        if '"device_id"' in line:
-            parts = json.loads(line[line.find('{'):line.rfind('}') + 1])
-            device = device_id_to_style_url(parts['device_id'])
-            if device in COLOR_MAP:
-                continue
-
-            if len(preferred_colors) > 0:
-                color = preferred_colors.pop()
-            else:
-                color = 'ffaaffff'
-            COLOR_MAP[device] = color
-
-
-def process_streams(in_stream, out_stream, name):
+def process_lines(in_stream):
+    """I don't know."""
     run_count = 1
     runs = []
     for line in in_stream:
         if 'Received run command' in line:
             print('Starting run {}'.format(run_count))
-            runs.append(process_run(in_stream, name, run_count))
+            runs.append(process_run(in_stream, run_count))
             run_count += 1
-
-    out_stream.write(
-        KML_TEMPLATE.format(
-            name=name,
-            style_maps='\n'.join((
-                STYLE_TEMPLATE.format(
-                    id=device_id_to_style_url(device),
-                    scale=1.2,
-                    color=color
-                ) for device, color in COLOR_MAP.items()
-            )),
-            folders=''.join(runs),
-        )
-    )
+    return runs
 
 
-def process_run(in_stream, name, run_count):
+def process_run(in_stream, run_count):
+    """Returns the points in a run."""
     points = collections.defaultdict(lambda: [])
     for line in in_stream:
         if 'Received stop command' in line or 'No waypoints, stopping' in line:
@@ -134,7 +51,7 @@ def process_run(in_stream, name, run_count):
             longitude = parts['longitude_d']
             # Ignore early bad estimates
             if latitude > 1:
-                points[parts['device_id']].append((longitude, latitude))
+                points[parts['device_id']].append((latitude, longitude))
             else:
                 print('Ignoring {},{}'.format(latitude, longitude))
 
@@ -144,28 +61,7 @@ def process_run(in_stream, name, run_count):
             len(points)
         )
     )
-
-    return FOLDER_TEMPLATE.format(
-        name=str(run_count),
-        placemark='\n'.join((
-            PLACEMARK_TEMPLATE.format(
-                name=device,
-                coordinates=' '.join((
-                    '{latitude},{longitude},0'.format(
-                        latitude=point[0],
-                        longitude=point[1]
-                    )
-                    for point in points[device]
-                )),
-                style_url='#m{id}'.format(id=device_id_to_style_url(device)),
-            )
-            for device in points
-        ))
-    )
-
-
-def device_id_to_style_url(device):
-    return device[:20].replace(' ', '-')
+    return points
 
 
 if __name__ == '__main__':
